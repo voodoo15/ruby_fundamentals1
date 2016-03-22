35f91b74d83fccda257f8848731401c375cb6153
Arnold Redoblado <arnold.redoblado@gmail.com>
2016-03-22 13:52:36 -0400

    Completed exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,5 +1,30 @@
+#Exercise 1
+
 puts 2
 puts 3
 puts 2 + 3
 
 #puts 2 puts 3 puts 2 + 3
+
+puts 'Hello world'
+puts "Hello world"
+
+puts "Betty's pie shop"
+#puts 'Betty\'s pie shop'
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+puts "Hello\t\world"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hellow\rStart over world"
+puts "1. Hello\n2.World"
+
+puts (1 < 3) && (3 < 5)
+
+puts (1 > 1) && (2 > 2)
+
+puts (1 == 2) || (2 < 3)
+
+puts (1 != 2) || (2 < 3)
+
+puts (1 == 2) || (2 == 3)
