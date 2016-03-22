7291956a50873dd1dcec954506d41271760e9fd4
Arnold Redoblado <arnold.redoblado@gmail.com>
2016-03-22 14:12:44 -0400

    Saved my guess to expression prior to running

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+meal_price = 55
+puts "The cost of the meal is:  $#{(meal_price).round(2)}\nThe tip is: $#{(meal_price * 0.15).round(2)}\nThe Total bill is:  $#{((meal_price * 1.13) + (meal_price * 0.15)).round(2)}"
+
+puts "I love " + " beer!"
+
+puts "I love $" + 100.to_s + " bills y'all!  Who got Benjamins?"
+
+puts "#{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#My guess is false
